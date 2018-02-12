a65b636e7b4d0fa3872a4f406bff4894e4a56c9e
Lord-Asriel <kyle.hogue19@alumni.loyola.ca>
2018-02-12 18:18:55 -0500

    We do what we must...Because we can

--- Schedule/Schedule.js
+++ Schedule/Schedule.js
@@ -1,4 +1,64 @@
-	/*var Math = "Miss Math";
+	
+			var BigMommy = 1;
+			var F = "You do realize that you are clicking in the wrong place right now? If you want to see more information about a class you can just go ask me! <br> If your that desperate you can just go use the <br> search-bar below."
+			function Happy() {
+			  document.getElementById("Table1").innerHTML = F
+			}
+			var Hananna = "Sorry Folks, if you want to search again, you got to refresh the page!"
+			function Kilooo() {
+			  document.getElementById("Table2").innerHTML = Hananna
+			}
+			
+			function DearHearts() {
+				switch (BigMommy) {
+					case 1 :
+						var typedClass = document.getElementById("SearchBar").value;
+						switch (typedClass){
+						case "French":
+							var Go = "French";
+							break;
+						case "Math":
+							var Go = "Math";
+							break;
+						case "English":
+							var Go = "English";
+							break;
+						case "Computer Programing":
+							var Go = "Computer-Programing";
+							break;
+						case "Theater Arts":
+							var Go = "Theater-Arts";
+							break;
+						case "Speech":
+							var Go = "Speech";
+							break;
+						case "Physical Education":
+							var Go = "Physical-Education";
+							break;
+						case "Advisors":
+							var Go = "Advisors";
+							break;
+						case "Religion":
+							var Go = "Religion";
+							break;
+						}
+						var LetsGo = document.getElementsByClassName(Go);
+						var i;
+						for (i = 0; i < LetsGo.length; i++) {
+						LetsGo[i].style.backgroundColor = "yellow";
+						var BigMommy = BigMommy + 1;
+						break;
+					case 2 :
+						Kilooo();
+						break;
+				}
+			}
+		
+		/* This section of the code features projects that I was not able to complete before the due date.
+		
+		Search by class
+		
+			var Math = "Miss Math";
 			var French = "Miss Baruthio";
 			var Speech = "Mr. Hanano";
 			var Theater-Arts = "Miss Eves";
@@ -18,13 +78,10 @@
 			document.getElementsByClassName("History").innerHTML = History;
 			document.getElementsByClassName("Advisors").innerHTML = Advisors;
 			document.getElementsByClassName("Physical-Education").innerHTML = Physical-Education;
-			document.getElementsByClassName("Religion").innerHTML = Religion;*/
-			var BigMommy = 1;
-			var F = "You do realize that you are clicking in the wrong place right now? If you want to see more information about a class you can just go ask me! <br> If your that desperate you can just go use the <br> search-bar below."
-			function Happy() {
-			  document.getElementById("Table1").innerHTML = F
-			}
-			/*
+			document.getElementsByClassName("Religion").innerHTML = Religion;
+			
+			Change Picture As Scroll Down
+			
 			window.addEventListener("scroll", TheoTheFunction); */
 			
 			/*function TheoTheFunction() {
@@ -40,37 +97,3 @@
 			}
 			var ThatsHowTheWeekendGoes =asdf
 			*/
-			function DearHearts() {
-				var typedClass = document.getElementById("SearchBar").value;
-				switch (typedClass){
-				case "French":
-					var Go = "French"
-					break;
-				case "Math":
-					console.log("Mac");
-					break;
-				case "English":
-					console.log("Mac");
-					break;
-				case "Computer Programing":
-					console.log("Mac");
-					break;
-				case "Theater Arts":
-					console.log("Mac");
-					break;
-				case "Speech":
-					console.log("Mac");
-					break;
-				case "Gym":
-					console.log("Mac");
-					break;
-				case "Advisors":
-					console.log("Mac");
-					break;
-				case "Religion":
-					console.log("Mac");
-					break;
-				}
-				
-			}
-		
