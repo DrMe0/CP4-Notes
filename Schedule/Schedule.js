86daa44e2b27a7d384f9ac4477dd0045de3a793e
Lord-Asriel <kyle.hogue19@alumni.loyola.ca>
2018-02-12 11:36:19 -0500

    Add files via upload

--- Schedule/Schedule.js
+++ Schedule/Schedule.js
@@ -1,21 +1,76 @@
-var M = "Miss Math";
-var F = "Miss Baruthio";
-var S = "Mr. Hanano";
-var T = "Miss Eves";
-var C = "Mr. Linux";
-var E = "Mr. Achillies";
-var H = "Mr. DnD";
-var L = "Mr. Fun";
-var A = "Mr. Help";
-var Speech = "Mr. DDDDDDD";
-var P = "Prince";
-document.getElementById("M").innerHTML = M;
-document.getElementById("F").innerHTML = F;
-document.getElementById("S").innerHTML = S;
-document.getElementById("T").innerHTML = T;
-document.getElementById("C").innerHTML = C;
-document.getElementById("E").innerHTML = E;
-document.getElementById("H").innerHTML = H;
-document.getElementById("L").innerHTML = L;
-document.getElementById("A").innerHTML = A;
-document.getElementById("P").innerHTML = P;
+	/*var Math = "Miss Math";
+			var French = "Miss Baruthio";
+			var Speech = "Mr. Hanano";
+			var Theater-Arts = "Miss Eves";
+			var Computer-Programing = "Mr. Linux";
+			var English = "Mr. Achillies";
+			var History = "Mr. DnD";
+			var Advisors = "Mr. Help";
+			var Physical-Education = "Mr. Fun";
+			var Speech = "Mr. DDDDDDD";
+			var Religion = "Mr. Hero"; 
+			document.getElementsByClassName("Math").innerHTML = Math;
+			document.getElementsByClassName("French").innerHTML = French;
+			document.getElementsByClassName("Speech").innerHTML = Speech;
+			document.getElementsByClassName("Theater-Arts").innerHTML = Theater-Arts;
+			document.getElementsByClassName("Computer-Programing").innerHTML = Computer-Programing;
+			document.getElementsByClassName("English").innerHTML = English;
+			document.getElementsByClassName("History").innerHTML = History;
+			document.getElementsByClassName("Advisors").innerHTML = Advisors;
+			document.getElementsByClassName("Physical-Education").innerHTML = Physical-Education;
+			document.getElementsByClassName("Religion").innerHTML = Religion;*/
+			var BigMommy = 1;
+			var F = "You do realize that you are clicking in the wrong place right now? If you want to see more information about a class you can just go ask me! <br> If your that desperate you can just go use the <br> search-bar below."
+			function Happy() {
+			  document.getElementById("Table1").innerHTML = F
+			}
+			/*
+			window.addEventListener("scroll", TheoTheFunction); */
+			
+			/*function TheoTheFunction() {
+				var scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+				while (BigMommy = 1) {
+					if (scrollpercent > 0) {
+						document.getElementbyId("SayIt").innerHTML = "Click Here To Search";
+						} 
+						else {
+						document.getElementbyId("SayIt").innerHTML = "Scroll Down";
+						}
+				}
+			}
+			var ThatsHowTheWeekendGoes =asdf
+			*/
+			function DearHearts() {
+				var typedClass = document.getElementById("SearchBar").value;
+				switch (typedClass){
+				case "French":
+					var Go = "French"
+					break;
+				case "Math":
+					console.log("Mac");
+					break;
+				case "English":
+					console.log("Mac");
+					break;
+				case "Computer Programing":
+					console.log("Mac");
+					break;
+				case "Theater Arts":
+					console.log("Mac");
+					break;
+				case "Speech":
+					console.log("Mac");
+					break;
+				case "Gym":
+					console.log("Mac");
+					break;
+				case "Advisors":
+					console.log("Mac");
+					break;
+				case "Religion":
+					console.log("Mac");
+					break;
+				}
+				
+			}
+		
