b1497f19c4594bb93cad159ff4323efda0a90a0a
Lord-Asriel <kyle.hogue19@alumni.loyola.ca>
2018-02-12 20:17:56 -0500

    Add files via upload
    Fixed tabbing and format.
    Stills errors to fix and problem with multiple highlights.
    Switch and highlighting works!
    Must comment more.

--- Schedule/Schedule.js
+++ Schedule/Schedule.js
@@ -1,99 +1,93 @@
-	
-			var BigMommy = 1;
-			var F = "You do realize that you are clicking in the wrong place right now? If you want to see more information about a class you can just go ask me! <br> If your that desperate you can just go use the <br> search-bar below."
-			function Happy() {
-			  document.getElementById("Table1").innerHTML = F
+var BigMommy = 1;
+var F = "You do realize that you are clicking in the wrong place right now? If you want to see more information about a class you can just go ask me! <br> If your that desperate you can just go use the <br> search-bar below."
+function Happy() {
+	 document.getElementById("Table1").innerHTML = F
+}
+var Hananna = "Sorry Folks, if you want to search again, you got to refresh the page!"
+function Kilooo() {
+	document.getElementById("Table2").innerHTML = Hananna
+}
+function DearHearts() {
+	switch (BigMommy) {
+		case 1 :
+			var typedClass = document.getElementById("SearchBar").value;
+			switch (typedClass){
+				case "French":
+					var Go = "French";
+				break;
+				case "Math":
+					var Go = "Math";
+				break;
+				case "English":
+					var Go = "English";
+				break;
+				case "Computer Programing":
+					var Go = "Computer-Programing";
+				break;
+				case "Theater Arts":
+					var Go = "Theater-Arts";
+				break;
+				case "Speech":
+					var Go = "Speech";
+				break;
+				case "Physical Education":
+					var Go = "Physical-Education";
+				break;
+				case "Advisors":
+					var Go = "Advisors";
+				break;
+				case "Religion":
+					var Go = "Religion";
+				break;
 			}
-			var Hananna = "Sorry Folks, if you want to search again, you got to refresh the page!"
-			function Kilooo() {
-			  document.getElementById("Table2").innerHTML = Hananna
-			}
-			
-			function DearHearts() {
-				switch (BigMommy) {
-					case 1 :
-						var typedClass = document.getElementById("SearchBar").value;
-						switch (typedClass){
-						case "French":
-							var Go = "French";
-							break;
-						case "Math":
-							var Go = "Math";
-							break;
-						case "English":
-							var Go = "English";
-							break;
-						case "Computer Programing":
-							var Go = "Computer-Programing";
-							break;
-						case "Theater Arts":
-							var Go = "Theater-Arts";
-							break;
-						case "Speech":
-							var Go = "Speech";
-							break;
-						case "Physical Education":
-							var Go = "Physical-Education";
-							break;
-						case "Advisors":
-							var Go = "Advisors";
-							break;
-						case "Religion":
-							var Go = "Religion";
-							break;
-						}
-						var LetsGo = document.getElementsByClassName(Go);
-						var i;
-						for (i = 0; i < LetsGo.length; i++) {
-						LetsGo[i].style.backgroundColor = "yellow";
-						var BigMommy = BigMommy + 1;
-						break;
-					case 2 :
-						Kilooo();
-						break;
-				}
-			}
+			var LetsGo = document.getElementsByClassName(Go);
+			var i;
+			for (i = 0; i < LetsGo.length; i++) {
+			LetsGo[i].style.backgroundColor = "yellow";
+			var BigMommy = BigMommy + 1;
+			break;
+		case 2 :
+			Kilooo();
+		break;
+	}
+}
+/* This section of the code features projects that I was not able to complete before the due date.
 		
-		/* This section of the code features projects that I was not able to complete before the due date.
+	Search by class
 		
-		Search by class
-		
-			var Math = "Miss Math";
-			var French = "Miss Baruthio";
-			var Speech = "Mr. Hanano";
-			var Theater-Arts = "Miss Eves";
-			var Computer-Programing = "Mr. Linux";
-			var English = "Mr. Achillies";
-			var History = "Mr. DnD";
-			var Advisors = "Mr. Help";
-			var Physical-Education = "Mr. Fun";
-			var Speech = "Mr. DDDDDDD";
-			var Religion = "Mr. Hero"; 
-			document.getElementsByClassName("Math").innerHTML = Math;
-			document.getElementsByClassName("French").innerHTML = French;
-			document.getElementsByClassName("Speech").innerHTML = Speech;
-			document.getElementsByClassName("Theater-Arts").innerHTML = Theater-Arts;
-			document.getElementsByClassName("Computer-Programing").innerHTML = Computer-Programing;
-			document.getElementsByClassName("English").innerHTML = English;
-			document.getElementsByClassName("History").innerHTML = History;
-			document.getElementsByClassName("Advisors").innerHTML = Advisors;
-			document.getElementsByClassName("Physical-Education").innerHTML = Physical-Education;
-			document.getElementsByClassName("Religion").innerHTML = Religion;
+	var Math = "Miss Math";
+	var French = "Miss Baruthio";
+	var Speech = "Mr. Hanano";
+	var Theater-Arts = "Miss Eves";
+	var Computer-Programing = "Mr. Linux";
+	var English = "Mr. Achillies";
+	var History = "Mr. DnD";
+	var Advisors = "Mr. Help";
+	var Physical-Education = "Mr. Fun";
+	var Speech = "Mr. DDDDDDD";
+	var Religion = "Mr. Hero"; 
+	document.getElementsByClassName("Math").innerHTML = Math;
+	document.getElementsByClassName("French").innerHTML = French;
+	document.getElementsByClassName("Speech").innerHTML = Speech;
+	document.getElementsByClassName("Theater-Arts").innerHTML = Theater-Arts;
+	document.getElementsByClassName("Computer-Programing").innerHTML = Computer-Programing;
+	document.getElementsByClassName("English").innerHTML = English;
+	document.getElementsByClassName("History").innerHTML = History;
+	document.getElementsByClassName("Advisors").innerHTML = Advisors;
+	document.getElementsByClassName("Physical-Education").innerHTML = Physical-Education;
+	document.getElementsByClassName("Religion").innerHTML = Religion;
 			
-			Change Picture As Scroll Down
+	Change Picture As Scroll Down
 			
-			window.addEventListener("scroll", TheoTheFunction); */
-			
-			/*function TheoTheFunction() {
-				var scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-				while (BigMommy = 1) {
-					if (scrollpercent > 0) {
-						document.getElementbyId("SayIt").innerHTML = "Click Here To Search";
-						} 
-						else {
-						document.getElementbyId("SayIt").innerHTML = "Scroll Down";
-						}
-				}
+	window.addEventListener("scroll", TheoTheFunction);
+	function TheoTheFunction() {
+		var scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+		while (BigMommy = 1) {
+			if (scrollpercent > 0) {
+				document.getElementbyId("SayIt").innerHTML = "Click Here To Search";
+			}else{
+				document.getElementbyId("SayIt").innerHTML = "Scroll Down";
 			}
-			var ThatsHowTheWeekendGoes =asdf
-			*/
+		}
+	}
+	var ThatsHowTheWeekendGoes =asdf*/
